ab2984de1f1c5d0ba4a31065d20b694026d0ae9b
Psan <nakahiroto0208@gmail.com>
2026-01-19 19:48:51 +0900

    chore: allowedHosts に本番ドメインを追加

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
 	server: {
 		port: 5174,
 		strictPort: true,
-		allowedHosts: ['blogtest.psannetwork.net']
+		allowedHosts: ['blogtest.psannetwork.net', 'blog.psannetwork.net']
 	}
 });
